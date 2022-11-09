c7d4a9b692a365b20086404c125358c1ae3ba277
ElrohirGT <elrohirgt@gmail.com>
2022-11-08 21:08:31 -0600

    fix:Clarified the example

--- EjemploRadio.java
+++ EjemploRadio.java
@@ -3,5 +3,9 @@ import java.util.ArrayList;
 public abstract class EjemploRadio implements IRadio {
     protected EjemploRadio(ArrayList<ArrayList<ICancion>> playlists, ArrayList<Float> emisorasGuardadas) {
         // Hacer algo con las playlists y emisoras que "el usuario ya guardó"
+        //
+        // Este es un ejemplo de constructor de una clase radio, es solo para facilitar
+        // la calificada del auxiliar al no tener que cambiar también la forma de
+        // creación del objeto.
     }
 }
