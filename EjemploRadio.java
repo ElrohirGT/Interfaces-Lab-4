169ec3ea167e48905238aff60ae35a9af22eef58
ElrohirGT <elrohirgt@gmail.com>
2022-11-08 20:50:41 -0600

    feat:Added an example of radio implementation

---
+++ EjemploRadio.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public abstract class EjemploRadio implements IRadio {
+    protected EjemploRadio(ArrayList<ArrayList<ICancion>> playlists, ArrayList<Float> emisorasGuardadas) {
+        // Hacer algo con las playlists y emisoras que "el usuario ya guardó"
+    }
+}
